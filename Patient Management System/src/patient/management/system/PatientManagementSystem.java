2f85ebdc878d2dfca57c8030163c32b0b777692b
rdoran1 <33032112+rdoran1@users.noreply.github.com>
2019-01-15 17:44:10 +0000

    Initial Push
    First Project Push

---
+++ Patient Management System/src/patient/management/system/PatientManagementSystem.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package patient.management.system;
+
+/**
+ *
+ * @author rdoran1
+ */
+public class PatientManagementSystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
